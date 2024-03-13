011446e32edfa64dc766f97cac737af5212db544
Amanda Limón <amandalimon@outlook.com>
2024-03-12 19:51:50 -0600

    type inference

---
+++ src/03-typing.ts
@@ -0,0 +1,13 @@
+// función anónima autoejecutable
+(() => {
+  let myProductName = 'Product 1';
+  let myProductPrice = 123;
+
+  myProductName = 'change';
+  myProductName.toLowerCase();
+
+  myProductPrice.toFixed();
+
+  const myProductStock = 1000;
+  const myProductName2 = 'Product 1';
+})();
